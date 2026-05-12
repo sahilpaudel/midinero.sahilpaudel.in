5d4a024954ce0a6e7c764a89c62c87e705b5362c
Sahil Paudel <thehighpaudel@Sahils-MacBook-Air.local>
2026-05-12 09:27:26 +0530

    fix chunk size limit warning

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173, open: true },
+  build: {
+    chunkSizeWarningLimit: 1400,
+  },
 });
